529c203111f273ef1ce2395396a837e0372ebfe2
hendrasoewarno <hendrasoewarno@gmail.com>
2026-05-14 20:34:05 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,22 @@
 const CACHE_NAME = 'pwa-auth-cache-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+
+// CHANGE THIS: Use paths relative to the sw.js location
+const ASSETS = [
+  './',
+  'index.html',
+  'manifest.json'
+];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting()) // Force immediate activation
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
 });
